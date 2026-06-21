a4fa8c38c996db806d1af5c7251480561b8ad364
twentytw <halbatech32@gmail.com>
2026-06-21 22:53:16 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Kronos/', // <-- ADD THIS LINE (Replace with your exact GitHub repository name)
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
